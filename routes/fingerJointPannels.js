17223f32c63069538c1dfe88275154a02f0dc337
samilkahraman <msamilkahraman@gmail.com>
2020-02-27 15:38:08 +0300

    Cors settings handled

--- routes/fingerJointPannels.js
+++ routes/fingerJointPannels.js
@@ -1,45 +1,46 @@
 var express = require("express");
 var router = express.Router();
 var prices = require("../public/price-list.json")
+var cors = require('cors');
 
 // middleware that is specific to this router
 router.use(function timeLog(req, res, next) {
   console.log("Time: ", Date.now());
   next();
 });
+router.use(cors());
 // define the home page route
 router.get("/", function(req, res) {
+  res.header("Access-Control-Allow-Headers","*");
   res.send(prices);
 });
 
-// var cb0 = function(req, res, next) {
-//     if (req.params.woodName && req.params.woodName == "cam") {
-//         if(req.params.quality && req.params.quality =="ab"){
-//             if(req.params.thickness && req.params.thickness=="18"){
-//                 if(req.params.solid &&)
-//             }
-//         }
-//         res.send(prices.cam);
-//     }
+router.use(express.static('public'))
 
-//     next();
-//   };
+var cb0 = function(req, res, next) {
+    if (req.params.woodName && req.params.woodName == "cam") {
+       
+        res.send(prices.cam);
+    }
+
+    next();
+  };
   
-//   var cb1 = function(req, res, next) {
-//     console.log("CB1");
-//     next();
-//   };
+  var cb1 = function(req, res, next) {
+    console.log("CB1");
+    next();
+  };
   
-//   var cb2 = function(req, res) {
-//     console.log("cb2")
-//     res.end()
-//   };
+  var cb2 = function(req, res) {
+    console.log("cb2")
+    res.end()
+  };
 // define the about route
-/*router.get("/pannels/:woodName/quality/:quality/thickness/:thickness/:solid", [
+router.get("/pannels/:woodName/quality/:quality/thickness/:thickness/:solid", [
   cb0,
   cb1,
   cb2
-]);*/
+]);
 
 
 
